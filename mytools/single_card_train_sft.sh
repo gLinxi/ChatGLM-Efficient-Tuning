62f66d2aab86e89034d4ecbc676430091d19b81c
gLinxi <gzxmling@foxmail.com>
2023-07-19 09:53:10 +0800

    update code

--- mytools/single_card_train_sft.sh
+++ mytools/single_card_train_sft.sh
@@ -8,11 +8,12 @@ model_base_dir="/root/share/LocalModelHub"
 # model_base_dir="/home/apps/gzx/LocalModelHub"
 # model_base_dir="/data/jupyterlab/gzx/LocalModelHub/"
 chatglm2_6b=${model_base_dir}"/chatglm2_6b/hf"
+chatglm_6b=${model_base_dir}"/chatglm_6b/hf"
 
 # params
 gpu_id=0
-model="jiutian_test_chatglm26b"
-sft_data="intention_train"
+model="test_chatglm6b"
+sft_data="tool_v2_train"
 output_dir="${model_base_dir}/"${model}"/ckp"
 log_dir="${model_base_dir}/"${model}"/log/"
 
@@ -21,8 +22,7 @@ mkdir -p ${log_dir}
 
 set -x
 CUDA_VISIBLE_DEVICES=${gpu_id} python ./src/train_sft.py \
-    --model_name_or_path ${chatglm2_6b} \
-    --use_v2 \
+    --model_name_or_path ${chatglm_6b} \
     --do_train \
     --dataset  ${sft_data}\
     --finetuning_type lora \
@@ -31,12 +31,12 @@ CUDA_VISIBLE_DEVICES=${gpu_id} python ./src/train_sft.py \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 16 \
     --lr_scheduler_type cosine \
-    --logging_steps 10 \
-    --save_steps 1000 \
+    --logging_steps 1 \
+    --save_steps 3000 \
     --warmup_steps 0 \
     --learning_rate 1e-3 \
     --max_source_length 2048 \
     --max_target_length 2048 \
     --ddp_find_unused_parameters False \
     --num_train_epochs 3.0 \
-    --fp16 > ${log_dir}/${date} 2>&1
+    --fp16
