9d4052b471ee21fd8c91d77f58eeffd38b7f0342
gLinxi <gzxmling@foxmail.com>
2023-07-19 10:26:04 +0800

    update script

--- mytools/export_model.sh
+++ mytools/export_model.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # params
-output_model="rec_reason_v1"
-checkpoint="./model/rec_reason_sft_checkpoint"
+output_model="rec_reason_v2_without_tag_chatglm6b"
 
 # default
 model_base_dir="/data/jupyterlab/gzx/LocalModelHub"
 chatglm_6b=${model_base_dir}"/chatglm_6b/hf"
+chatglm2_6b=${model_base_dir}"/chatglm2_6b/hf"
 output_dir=${model_base_dir}"/${output_model}/hf"
+checkpoint=${model_base_dir}"/${output_model}/ckp"
 
+mkdir -p ${output_dir}
+
+set -x
 python src/export_model.py \
 	--model_name_or_path ${chatglm_6b} \
         --checkpoint_dir ${checkpoint} \
 	--output_dir ${output_dir}
+
+cp ${chatglm_6b}/*.py ${output_dir}
